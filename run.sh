1bad8702d526e817367435b16b9a69bfc5774e52
James Yoo <jyoo980@gmail.com>
2025-10-16 13:27:03 -0700

    Adding back required changes

--- run.sh
+++ run.sh
@@ -1,8 +1,21 @@
+# Unset this value if running on CI.
+INTERACTIVE="-it"
+
+if [ -n "${CI:-}" ] || [ ! -t 0 ]; then
+    echo "Running in a non-interactive environment"
+    INTERACTIVE=""
+fi
+
+CMD=(/bin/bash)
+if [ "$#" -gt 0 ]; then
+    CMD=("$@")
+fi
+
 # Check if Docker is rootless.
 # If so, run the container with root user to avoid permission issues with mounted volumes.
 if docker info -f "{{println .SecurityOptions}}" | grep -q rootless; then
     echo "Docker running in rootless mode"
-    docker run --rm -u 0:0 -v $(pwd)/data:/app/data \
+    docker run --rm $INTERACTIVE -u 0:0 -v $(pwd)/data:/app/data \
                            -v $(pwd)/docs:/app/docs \
                            -v $(pwd)/models:/app/models \
                            -v $(pwd)/test:/app/test \
@@ -10,9 +23,9 @@ if docker info -f "{{println .SecurityOptions}}" | grep -q rootless; then
                            -v $(pwd)/specs:/app/specs \
                            -v $(pwd)/prompt.txt:/app/prompt.txt \
                            -v $(pwd)/generate_specs.py:/app/generate_specs.py \
-                           -it cbmc:latest /bin/bash
+                           cbmc:latest "${CMD[@]}"
 else
-    docker run --rm -v $(pwd)/data:/app/data \
+    docker run --rm $INTERACTIVE -v $(pwd)/data:/app/data \
                     -v $(pwd)/docs:/app/docs \
                     -v $(pwd)/models:/app/models \
                     -v $(pwd)/test:/app/test \
@@ -20,5 +33,5 @@ else
                     -v $(pwd)/specs:/app/specs \
                     -v $(pwd)/prompt.txt:/app/prompt.txt \
                     -v $(pwd)/generate_specs.py:/app/generate_specs.py \
-                    -it cbmc:latest /bin/bash
+                    cbmc:latest "${CMD[@]}"
 fi
