b8e8567d4c2aa96811316371f4adf4438abfd8e6
James Yoo <24359440+jyoo980@users.noreply.github.com>
2025-10-19 15:26:50 -0700

    Quoting paths in Docker run commands (#16)

--- run.sh
+++ run.sh
@@ -15,23 +15,23 @@ fi
 # If so, run the container with root user to avoid permission issues with mounted volumes.
 if docker info -f "{{println .SecurityOptions}}" | grep -q rootless; then
     echo "Docker running in rootless mode"
-    docker run --rm $INTERACTIVE -u 0:0 -v $(pwd)/data:/app/data \
-                           -v $(pwd)/docs:/app/docs \
-                           -v $(pwd)/models:/app/models \
-                           -v $(pwd)/test:/app/test \
-                           -v $(pwd)/util:/app/util \
-                           -v $(pwd)/specs:/app/specs \
-                           -v $(pwd)/prompt.txt:/app/prompt.txt \
-                           -v $(pwd)/generate_specs.py:/app/generate_specs.py \
+    docker run --rm $INTERACTIVE -u 0:0 -v "$(pwd)/data:/app/data" \
+                           -v "$(pwd)/docs:/app/docs" \
+                           -v "$(pwd)/models:/app/models" \
+                           -v "$(pwd)/test:/app/test" \
+                           -v "$(pwd)/util:/app/util" \
+                           -v "$(pwd)/specs:/app/specs" \
+                           -v "$(pwd)/prompt.txt:/app/prompt.txt" \
+                           -v "$(pwd)/generate_specs.py:/app/generate_specs.py" \
                            cbmc:latest "${CMD[@]}"
 else
-    docker run --rm $INTERACTIVE -v $(pwd)/data:/app/data \
-                    -v $(pwd)/docs:/app/docs \
-                    -v $(pwd)/models:/app/models \
-                    -v $(pwd)/test:/app/test \
-                    -v $(pwd)/util:/app/util \
-                    -v $(pwd)/specs:/app/specs \
-                    -v $(pwd)/prompt.txt:/app/prompt.txt \
-                    -v $(pwd)/generate_specs.py:/app/generate_specs.py \
+    docker run --rm $INTERACTIVE -v "$(pwd)/data:/app/data" \
+                    -v "$(pwd)/docs:/app/docs" \
+                    -v "$(pwd)/models:/app/models" \
+                    -v "$(pwd)/test:/app/test" \
+                    -v "$(pwd)/util:/app/util" \
+                    -v "$(pwd)/specs:/app/specs" \
+                    -v "$(pwd)/prompt.txt:/app/prompt.txt" \
+                    -v "$(pwd)/generate_specs.py:/app/generate_specs.py" \
                     cbmc:latest "${CMD[@]}"
 fi
