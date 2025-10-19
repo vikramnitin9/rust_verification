440e0a3a0ac26ee811a86f6d42465be0e0dd7ae4
James Yoo <jyoo980@gmail.com>
2025-10-19 15:49:55 -0700

    Adding back `translation` module to Docker

--- run.sh
+++ run.sh
@@ -19,6 +19,7 @@ if docker info -f "{{println .SecurityOptions}}" | grep -q rootless; then
                            -v "$(pwd)/docs:/app/docs" \
                            -v "$(pwd)/models:/app/models" \
                            -v "$(pwd)/test:/app/test" \
+                           -v "$(pwd)/translation:/app/translation" \
                            -v "$(pwd)/util:/app/util" \
                            -v "$(pwd)/specs:/app/specs" \
                            -v "$(pwd)/prompt.txt:/app/prompt.txt" \
@@ -29,6 +30,7 @@ else
                     -v "$(pwd)/docs:/app/docs" \
                     -v "$(pwd)/models:/app/models" \
                     -v "$(pwd)/test:/app/test" \
+                    -v "$(pwd)/translation:/app/translation" \
                     -v "$(pwd)/util:/app/util" \
                     -v "$(pwd)/specs:/app/specs" \
                     -v "$(pwd)/prompt.txt:/app/prompt.txt" \
