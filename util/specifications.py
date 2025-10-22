48bc64a9cddb52e63116fe4ce552fa7e2bfb4cd7
James Yoo <jyoo980@gmail.com>
2025-10-22 09:39:36 -0700

    Documenting `specifications.py`

--- util/specifications.py
+++ util/specifications.py
@@ -4,6 +4,12 @@ from typing import Iterator
 
 @dataclass
 class Specifications:
+    """Represents the pre and postconditions of a function, if present.
+
+    Attributes:
+        preconditions (list[str]): The preconditions of the function.
+        postconditions (list[str]): The postconditions of the function.
+    """
     preconditions: list[str]
     postconditions: list[str]
 
