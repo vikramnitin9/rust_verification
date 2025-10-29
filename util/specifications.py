9dcc7cebfdc73afb306c5ef403f58685d7e9b756
James Yoo <jyoo980@gmail.com>
2025-10-29 13:00:31 -0700

    Documenting use-case for `FunctionSpecification#__iter__`

--- util/specifications.py
+++ util/specifications.py
@@ -16,9 +16,13 @@ class FunctionSpecification:
     preconditions: list[str]
     postconditions: list[str]
 
-    # TODO: What is the point of this?
     def __iter__(self) -> Iterator[list[str]]:
         """Return a singleton iterator that yields a list this specification's clauses.
+
+        This function allows tuple-based unpacking and assignments, e.g.,
+
+            function_spec = ...
+            pre, post = function_spec
 
         Returns:
             Iterator[list[str]]: An iterator comprising a tuple of pre and postconditions.
