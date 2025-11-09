2d01ecda0f25b18467b541088a52de7408a93bb4
James Yoo <jyoo980@gmail.com>
2025-11-09 11:05:33 -0800

    Adding tests for function updates

---
+++ test/data/function_util/update_function_declaration/swap_middle_with_specs.c
@@ -0,0 +1,17 @@
+void foo() {}
+
+void swap(int* a, int* b)
+__CPROVER_requires(__CPROVER_is_fresh(a, sizeof(int)))
+__CPROVER_requires(__CPROVER_is_fresh(b, sizeof(int)))
+__CPROVER_ensures(*a == __CPROVER_old(*b))
+__CPROVER_ensures(*b == __CPROVER_old(*a))
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int main()
+{
+    return 0;
+}
---
+++ test/data/function_util/update_function_declaration/swap_top.c
@@ -0,0 +1,11 @@
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int main()
+{
+    return 0;
+}
