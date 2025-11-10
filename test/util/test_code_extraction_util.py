b2e904aca0a5833f989d2227a34971451f681437
James Yoo <jyoo980@gmail.com>
2025-11-10 09:46:13 -0800

    Adding test case

--- test/util/test_code_extraction_util.py
+++ test/util/test_code_extraction_util.py
@@ -2,21 +2,27 @@ from util import code_extraction_util
 
 import pytest
 
-def test_invalid_json() -> None:
+
+def test_extract_function_invalid_json() -> None:
     text = '{ "function_with_specs": " }'
     with pytest.raises(RuntimeError):
         code_extraction_util.extract_function(text)
 
-def test_json_inside_code_fences() -> None:
+
+def test_extract_function_json_inside_code_fences() -> None:
     text = """```json
     {
         "function_with_specs": "#include <stdio.h> int main() { printf(\\"Hello, world\\"); }"
     }
     ```
     """
-    assert code_extraction_util.extract_function(text) == '#include <stdio.h> int main() { printf("Hello, world"); }'
+    assert (
+        code_extraction_util.extract_function(text)
+        == '#include <stdio.h> int main() { printf("Hello, world"); }'
+    )
 
-def test_json_inside_code_fences_with_whitespace() -> None:
+
+def test_extract_function_json_inside_code_fences_with_whitespace() -> None:
     text = """
     
     
@@ -27,6 +33,9 @@ def test_json_inside_code_fences_with_whitespace() -> None:
     }
     ```
 
-    
+
     """
-    assert code_extraction_util.extract_function(text) == '#include <stdio.h> int main() { printf("Hello, world"); }'
+    assert (
+        code_extraction_util.extract_function(text)
+        == '#include <stdio.h> int main() { printf("Hello, world"); }'
+    )
