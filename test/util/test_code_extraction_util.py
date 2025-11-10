39e183fdb32caf0469c3989afb0dd11da328c086
James Yoo <jyoo980@gmail.com>
2025-11-09 16:40:11 -0800

    Introducing explicit output format for LLM

--- test/util/test_code_extraction_util.py
+++ test/util/test_code_extraction_util.py
@@ -2,72 +2,31 @@ from util import code_extraction_util
 
 import pytest
 
-def test_empty_function_tags() -> None:
-    text = """<FUNC></FUNC>"""
+def test_invalid_json() -> None:
+    text = '{ "function_with_specs": " }'
     with pytest.raises(RuntimeError):
         code_extraction_util.extract_function(text)
 
-def test_empty_code_fences() -> None:
-    text = """
-    <FUNC>
-    ```C
-    ```
-    </FUNC>
-    """
-    with pytest.raises(RuntimeError):
-        code_extraction_util.extract_function(text)
-
-def test_valid_code_fence() -> None:
-    text = """
-    <FUNC>
-    ```C
-    #include <stdio.h>
-
-    int main()
+def test_json_inside_code_fences() -> None:
+    text = """```json
     {
-        printf("Hello, world!");
+        "function_with_specs": "#include <stdio.h> int main() { printf(\\"Hello, world\\"); }"
     }
     ```
-    </FUNC>
     """
-    assert code_extraction_util.extract_function(text) == """#include <stdio.h>
+    assert code_extraction_util.extract_function(text) == '#include <stdio.h> int main() { printf("Hello, world"); }'
 
-    int main()
-    {
-        printf("Hello, world!");
-    }"""
-
-def test_valid_code_fence_not_c() -> None:
+def test_json_inside_code_fences_with_whitespace() -> None:
     text = """
-    <FUNC>
-    ```scala
-    @main def(): Unit = println("Hello world!")
-    ```
-    </FUNC>
-    """
-    assert code_extraction_util.extract_function(text, code_fence_language="scala") == '@main def(): Unit = println("Hello world!")'
-
-def test_multiple_code_fence_invalid() -> None:
-    text = """
-    <FUNC>
-    ```c
-    #include <stdio.h>
-
-    int main()
+    
+    
+    
+    ```json
     {
-        printf("Hello, world!");
+        "function_with_specs": "#include <stdio.h> int main() { printf(\\"Hello, world\\"); }"
     }
     ```
 
-    ```C
-    #include <stdio.h>
-
-    int main()
-    {
-        printf("Hello, world!");
-    }
-    ```
-    </FUNC>
+    
     """
-    with pytest.raises(RuntimeError):
-        code_extraction_util.extract_function(text)
+    assert code_extraction_util.extract_function(text) == '#include <stdio.h> int main() { printf("Hello, world"); }'
