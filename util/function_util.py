1b87d8737e2ca04d40013620139c60ba16b6dcaa
James Yoo <jyoo980@gmail.com>
2025-11-28 11:06:28 -0800

    Adding functions for inserting specifications

--- util/function_util.py
+++ util/function_util.py
@@ -2,6 +2,9 @@
 
 from pathlib import Path
 
+import tree_sitter_c as tsc
+from tree_sitter import Language, Parser, Query, QueryCursor
+
 from .parsec_result import ParsecResult
 from .specifications import FunctionSpecification
 
@@ -30,6 +33,70 @@ def extract_specification(function_lines: list[str]) -> FunctionSpecification:
         preconditions=preconditions,
         postconditions=postconditions,
     )
+
+
+def get_signature_and_body(src: str, lang: str) -> tuple[str, str]:
+    """Return the signature and body of a function.
+
+    The first element of the tuple comprises the signature, the second element comprises the body.
+
+    Args:
+        src (str): The source code of the function.
+        lang (str): The language of the source function.
+
+    Returns:
+        tuple[str, str]: The signature and the body of a function.
+    """
+    if lang != "c":
+        msg = f"Unsupported language: {lang}"
+        raise RuntimeError(msg)
+    ts_lang = Language(tsc.language())
+    parser = Parser(ts_lang)
+    tree = parser.parse(bytes(src, encoding="utf-8"))
+    query = Query(
+        ts_lang,
+        """
+        (function_definition
+          body: (compound_statement) @function.body) @function.definition
+        """,
+    )
+    query_cursor = QueryCursor(query)
+    captures = query_cursor.captures(tree.root_node)
+
+    definition_node = captures["function.definition"][0]
+    body_node = captures["function.body"][0]
+
+    signature = src[definition_node.start_byte : body_node.start_byte].strip()
+    body = body_node.text.decode(encoding="utf-8")
+    return (signature, body)
+
+
+def get_source_code_with_specs(
+    function_name: str, specifications: FunctionSpecification, parsec_result: ParsecResult
+) -> str:
+    """Return the source code of a function with the specifications inserted.
+
+    Note: This does *not* update the ParsecResult with the specified function declaration.
+
+    Args:
+        function_name (str): The name of the function for which to return the updated source code.
+        specifications (FunctionSpecification): The specifications for the function.
+        parsec_result (ParsecResult): The ParsecResult.
+
+    Raises:
+        RuntimeError: Raised when the function is missing from the ParsecResult.
+
+    Returns:
+        str: The source code of a function with the specifications inserted.
+    """
+    if function := parsec_result.get_function(function_name=function_name):
+        (signature, body) = get_signature_and_body(function.get_source_code(), lang="c")
+        specs = "\n".join(
+            spec for spec in specifications.preconditions + specifications.postconditions
+        )
+        return f"{signature}\n{specs}\n{body}"
+    msg = f"{function_name} missing from ParsecResult"
+    raise RuntimeError(msg)
 
 
 def update_function_declaration(
