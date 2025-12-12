8b315cb0edd626856f381160eb35166893ace7df
James Yoo <jyoo980@gmail.com>
2025-12-12 13:04:09 -0800

    Building out `main.py`

---
+++ main.py
@@ -0,0 +1,84 @@
+"""Main entry point for specification generation and verification."""
+
+# !/opt/miniconda3/bin/python
+
+import argparse
+from pathlib import Path
+
+from loguru import logger
+
+from util import (
+    FunctionSpecification,
+    ParsecFunction,
+    ParsecResult,
+    copy_file_to_folder,
+    insert_lines_at_beginning,
+)
+
+MODEL = "gpt-4o"
+DEFAULT_HEADERS_IN_OUTPUT = ["stdlib.h", "limits.h"]
+DEFAULT_NUM_SPECIFICATION_CANDIDATES = 1
+DEFAULT_NUM_SPECIFICATION_REPAIR_ITERATIONS = 10
+DEFAULT_MODEL_TEMPERATURE = 1.0
+DEFAULT_SYSTEM_PROMPT = Path("prompts/system-prompt.txt").read_text(encoding="utf-8")
+
+
+def main() -> None:
+    """Generate specifications for C functions using an LLM and verify them with CBMC."""
+    parser = argparse.ArgumentParser(
+        prog="main.py", description="Generate and verify CBMC specifications for a C file."
+    )
+    parser.add_argument(
+        "--file",
+        required=True,
+        help="Path to the C file for which to generate and verify specifications.",
+    )
+    parser.add_argument(
+        "--num-specification-candidates",
+        required=False,
+        help="The number of candidate specifications to generate for a function.",
+        default=DEFAULT_NUM_SPECIFICATION_CANDIDATES,
+        type=int,
+    )
+    parser.add_argument(
+        "--num-specification-repair-iterations",
+        required=False,
+        help="The number of times to attempt to repair a faulty specification.",
+        default=DEFAULT_NUM_SPECIFICATION_REPAIR_ITERATIONS,
+        type=int,
+    )
+    args = parser.parse_args()
+
+    input_file_path = Path(args.file)
+    output_file_path = copy_file_to_folder(input_file_path, "specs")
+    header_lines = [f"#include <{header}>" for header in DEFAULT_HEADERS_IN_OUTPUT]
+    insert_lines_at_beginning(header_lines, output_file_path)
+
+    functions_in_reverse_topological_order = _get_functions_in_reverse_topological_order(
+        input_file_path
+    )
+    _verify_program(functions_in_reverse_topological_order)
+
+
+if __name__ == "__main__":
+    main()
+
+
+def _get_functions_in_reverse_topological_order(input_file_path: Path) -> list[ParsecFunction]:
+    parsec_result = ParsecResult(input_file_path)
+    # Get a list of functions in reverse topological order,
+    # i.e., starting from the leaves of the call graph.
+    function_names = parsec_result.copy(
+        remove_self_edges_in_call_graph=True
+    ).get_function_names_in_topological_order(reverse_order=True)
+    functions: list[ParsecFunction] = []
+    for function_name in function_names:
+        if function := parsec_result.get_function(function_name):
+            functions.append(function)
+        else:
+            logger.warning(f"Function '{function_name}' was missing from the ParsecResult")
+    return functions
+
+
+def _verify_program(functions: list[ParsecFunction]) -> dict[str, FunctionSpecification]:
+    raise NotImplementedError("Implement me")
