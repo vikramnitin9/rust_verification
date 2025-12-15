41bc201d1e0a77cd61e508bbfbe04ccd349e7500
Michael Ernst <mernst@cs.washington.edu>
2025-12-15 09:51:59 -0800

    Suppress a warning

--- translation/specifications/cbmc_to_kani.py
+++ translation/specifications/cbmc_to_kani.py
@@ -49,7 +49,7 @@ class CBMCToKani:
             try:
                 cbmc_ast = self.parser.parse(spec)
                 kani_specs.append(self._to_kani_str(cbmc_ast))
-            except UnexpectedToken as ut:
+            except UnexpectedToken as ut:  # noqa: PERF203
                 logger.error(f"Failed to parse: '{spec}' with error: '{ut}'")
                 continue
             except TranslationError as te:
