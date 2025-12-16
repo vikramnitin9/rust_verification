2bd3a63cea305c81ef2cc06146555956ab3b2172
Vikram Nitin <vikramnitin9@gmail.com>
2025-12-16 00:39:23 -0500

    Added Parsec as a submodule

--- parsec/FunctionVisitorConsumer.cpp
+++
@@ -1,9 +0,0 @@
-#include "FunctionVisitorConsumer.h"
-
-using namespace clang;
-
-void FunctionVisitorConsumer::HandleTranslationUnit(ASTContext &context) {
-    visitor.TraverseDecl(context.getTranslationUnitDecl());
-    data = visitor.getData();
-    defaultConsumer->HandleTranslationUnit(context);
-}
