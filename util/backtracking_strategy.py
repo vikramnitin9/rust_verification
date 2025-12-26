f1db44f22f59a29bc94f1f0e27608a5fd42d62d9
James Yoo <jyoo980@gmail.com>
2025-12-26 15:08:07 -0800

    Adding `is_regenerate_strategy` property to `BacktrackingStrategy`

--- util/backtracking_strategy.py
+++ util/backtracking_strategy.py
@@ -9,3 +9,17 @@ class BacktrackingStrategy(StrEnum):
     ASSUME_SPEC = "ASSUME_SPEC"
     REPAIR_SPEC = "REPAIR_SPEC"
     REGENERATE_CALLEE_SPEC = "REGENERATE_CALLEE_SPEC"
+
+    @property
+    def is_regenerate_strategy(self) -> bool:
+        """True iff this strategy involves specification regeneration.
+
+        For now, the only strategies that involve specification regeneration are REPAIR_SPEC and
+        REGENERATE_CALLEE_SPEC.
+
+        Returns:
+            bool: True iff this strategy involves the regeneration of a specification.
+        """
+        return self in frozenset(
+            {BacktrackingStrategy.REPAIR_SPEC, BacktrackingStrategy.REGENERATE_CALLEE_SPEC}
+        )
