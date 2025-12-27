2ff9e609a2d037fb4cb35c4069d904b26984ea72
James Yoo <jyoo980@gmail.com>
2025-12-27 15:08:16 -0800

    Introducing `ConversationMessage` datatype

---
+++ models/conversation_message.py
@@ -0,0 +1,54 @@
+"""Classes to represent messages in an LLM conversation."""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class ConversationMessage:
+    """Represents a message (either from an LLM or a user) in a conversation.
+
+    Attributes:
+        role (str): Denotes the provenance of a message (e.g,, "user", "system").
+        content (str): The content of a message (i.e., the actual prompt).
+    """
+
+    role: str
+    content: str
+
+    def to_dict(self) -> dict[str, str]:
+        """Return a dictionary representation of this conversation message.
+
+        Returns:
+            dict[str, str]: A dictionary representation of this conversation message.
+        """
+        return {"role": self.role, "content": self.content}
+
+
+@dataclass
+class UserMessage(ConversationMessage):
+    """Represents a message originating from a user (i.e., non-LLM entity) in a conversation."""
+
+    def __init__(self, content: str) -> None:
+        """Create a UserMessage."""
+        self.role = "user"
+        self.content = content
+
+
+@dataclass
+class LlmMessage(ConversationMessage):
+    """Represents a message sent by an LLM in a conversation."""
+
+    def __init__(self, content: str) -> None:
+        """Create a LlmMessage."""
+        self.role = "assistant"
+        self.content = content
+
+
+@dataclass
+class SystemMessage(ConversationMessage):
+    """Represents a message meant to be a system prompt in a conversation."""
+
+    def __init__(self, content: str) -> None:
+        """Create a SystemMessage."""
+        self.role = "system"
+        self.content = content
