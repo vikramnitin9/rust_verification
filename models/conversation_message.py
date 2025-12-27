5bc86c54a0496c5f9fadd474d7e6d13a7a94c983
James Yoo <jyoo980@gmail.com>
2025-12-27 15:35:04 -0800

    Using dataclasses properly

--- models/conversation_message.py
+++ models/conversation_message.py
@@ -1,28 +1,25 @@
 """Classes to represent messages in an LLM conversation."""
 
-import abc
 from dataclasses import dataclass
 
 
-@dataclass
-class ConversationMessage(abc.ABC):
-    """Represents a message (either from an LLM or a user) in a conversation.
+@dataclass(frozen=True)
+class ConversationMessage:
+    """Represents a message in a conversation.
 
     Attributes:
-        role (str): Denotes the provenance of a message (e.g., "user", "system").
         content (str): The content of a message (i.e., the actual prompt).
+        role (str): Denotes the provenance of a message (e.g., "user", "system").
     """
 
-    role: str
     content: str
+    role: str = ""
 
-    @abc.abstractmethod
-    def __init__(self):
-        """Create a ConversationMessage.
-
-        Note: This is a no-op init method explicitly marked with @abstractmethod to disallow
-        instantiation of this abstract parent class.
-        """
+    def __post_init__(self) -> None:
+        """Prevent direct instantiation of ConversationMessage."""
+        if type(self) is ConversationMessage:
+            msg = "'ConversationMessage' should be treated as an abstract class"
+            raise TypeError(msg)
 
     def to_dict(self) -> dict[str, str]:
         """Return a dictionary representation of this conversation message.
@@ -33,28 +30,22 @@ class ConversationMessage(abc.ABC):
         return {"role": self.role, "content": self.content}
 
 
+@dataclass(frozen=True)
 class UserMessage(ConversationMessage):
     """Represents a message originating from a user (i.e., non-LLM entity) in a conversation."""
 
-    def __init__(self, content: str) -> None:
-        """Create a UserMessage."""
-        self.role = "user"
-        self.content = content
+    role: str = "user"
 
 
+@dataclass(frozen=True)
 class LlmMessage(ConversationMessage):
     """Represents a message sent by an LLM in a conversation."""
 
-    def __init__(self, content: str) -> None:
-        """Create a LlmMessage."""
-        self.role = "assistant"
-        self.content = content
+    role: str = "assistant"
 
 
+@dataclass(frozen=True)
 class SystemMessage(ConversationMessage):
     """Represents a message meant to be a system prompt in a conversation."""
 
-    def __init__(self, content: str) -> None:
-        """Create a SystemMessage."""
-        self.role = "system"
-        self.content = content
+    role: str = "system"
