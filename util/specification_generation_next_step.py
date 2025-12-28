9e7946edd7aa3376b8fa72b4c20cd875c6ba2ffb
James Yoo <jyoo980@gmail.com>
2025-12-28 13:40:38 -0800

    Fix

---
+++ util/specification_generation_next_step.py
@@ -0,0 +1,34 @@
+"""Enums to represent the next step to take during specification generation."""
+
+from enum import StrEnum
+
+
+class SpecificationGenerationNextStep(StrEnum):
+    """Enum to represent the next step to take during specification generation."""
+
+    ACCEPT_VERIFIED_SPEC = "ACCEPT_VERIFIED_SPEC"
+    ASSUME_SPEC_AS_IS = "ASSUME_SPEC_AS_IS"
+    REPAIR_SPEC = "REPAIR_SPEC"
+    REGENERATE_CALLEE_SPEC = "REGENERATE_CALLEE_SPEC"
+
+    @property
+    def is_regenerate_strategy(self) -> bool:
+        """True iff this strategy involves specification regeneration.
+
+        Note: A specification is said to be "regenerated" if the current specification is not
+        accepted as-is by a verifier. A specification may be accepted as-is by a verifier if it
+        successfully verifies, or is assumed.
+
+        For now, the only strategies that involve specification regeneration are REPAIR_SPEC and
+        REGENERATE_CALLEE_SPEC.
+
+        Returns:
+            bool: True iff this strategy involves the regeneration of a specification.
+
+        """
+        return self in frozenset(
+            {
+                SpecificationGenerationNextStep.REPAIR_SPEC,
+                SpecificationGenerationNextStep.REGENERATE_CALLEE_SPEC,
+            }
+        )
