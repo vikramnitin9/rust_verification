f8e6b686c33e91a18f817bc004b3e773af6ee727
Michael Ernst <mernst@cs.washington.edu>
2026-01-06 15:41:18 -0800

    Improving naming of tests in `test_code_extraction_util.py`

--- test/util/test_code_extraction_util.py
+++ test/util/test_code_extraction_util.py
@@ -3,13 +3,13 @@ import pytest
 from util import code_extraction_util
 
 
-def test_extract_function_invalid_json() -> None:
+def test_extract_function_source_code_invalid_json() -> None:
     text = '{ "function_with_specs": " }'
     with pytest.raises(RuntimeError):
         code_extraction_util.extract_function_source_code(text)
 
 
-def test_extract_function_json_inside_code_fences() -> None:
+def test_extract_function_source_code_json_inside_code_fences() -> None:
     text = """```json
     {
         "function_with_specs": "#include <stdio.h> int main() { printf(\\"Hello, world\\"); }"
@@ -22,7 +22,7 @@ def test_extract_function_json_inside_code_fences() -> None:
     )
 
 
-def test_extract_function_json_inside_code_fences_with_whitespace() -> None:
+def test_extract_function_source_code_json_inside_code_fences_with_whitespace() -> None:
     text = """
     
     
@@ -41,7 +41,7 @@ def test_extract_function_json_inside_code_fences_with_whitespace() -> None:
     )
 
 
-def test_extract_function_plain_json() -> None:
+def test_extract_function_source_code_plain_json() -> None:
     text = """
     {
         "function_with_specs": "#include <stdio.h> int main() { printf(\\"Hello, world\\"); }"
