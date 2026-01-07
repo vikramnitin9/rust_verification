23e2da33f2b43f0b9bb8b1f343fd95b16660a694
James Yoo <jyoo980@gmail.com>
2026-01-06 20:41:24 -0800

    Note issue about generating just specs and not the entire function

--- util/code_extraction_util.py
+++ util/code_extraction_util.py
@@ -2,23 +2,23 @@
 
 from .json_util import parse_object
 
-# MDE: Did we discuss having the LLM return only the specs, without repeating the function's source
-# code?
-
 
 def extract_function_source_code(text: str) -> str:
     """Extract the source code part of an LLM response.
 
     An LLM is prompted to return a string in the following JSON format:
 
-    # TODO: just ask for the specification.  Then the LLM is less likely to hallucinate a
-    # change in the function's source code.
-    # Also make this more structured, such as separating the pre- and post-conditions.
-    # See https://github.com/vikramnitin9/rust_verification/issues/66
-
         { "function_with_specs": "<SOURCE CODE>" }
 
     This function extracts the <SOURCE CODE> part.
+
+    An alternate implementation would be to have the LLM generate *just* the specifications
+    (i.e., avoid having it generate the original function, as well). The initial implementation
+    asked the LLM to re-generate the entire function, so we kept it as such.
+
+    An issue to possibly implement the future approach is open at:
+
+        https://github.com/vikramnitin9/rust_verification/issues/66
 
     Args:
         text (str): The full response from an LLM.
