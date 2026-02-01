226b3d3ca0d51cc9e149da8f4ca74373542fbe04
James Yoo <jyoo980@gmail.com>
2026-01-31 16:32:26 -0800

    Adding cache reading util

---
+++ read_cache.py
@@ -0,0 +1,20 @@
+# ruff: noqa
+# !/opt/miniconda3/bin/python
+
+from diskcache import Cache  # ty: ignore
+
+VERIFIER_CACHE_PATH = "data/caching/verifier"
+
+
+def main() -> None:
+    verifier_cache = Cache(directory=VERIFIER_CACHE_PATH)
+    for key in verifier_cache.iterkeys():
+        vresult = verifier_cache[key]
+        if vresult.succeeded:
+            verified_spec = vresult.get_spec()
+            print(f"Function name = '{vresult.get_function().name}'")
+            print(f"Spec = {verified_spec}")
+
+
+if __name__ == "__main__":
+    main()
--- run.sh
+++ run.sh
@@ -40,6 +40,7 @@ if docker info -f "{{println .SecurityOptions}}" | grep -q rootless; then
     -v "$(pwd)/logs:/app/logs" \
     -v "$(pwd)/prompts:/app/prompts" \
     -v "$(pwd)/main.py:/app/main.py" \
+    -v "$(pwd)/read_cache.py:/app/read_cache.py" \
     -v "$(pwd)/translate_specs.py:/app/translate_specs.py" \
     cbmc:latest "${CMD[@]}"
 else
@@ -56,6 +57,7 @@ else
     -v "$(pwd)/logs:/app/logs" \
     -v "$(pwd)/prompts:/app/prompts" \
     -v "$(pwd)/main.py:/app/main.py" \
+    -v "$(pwd)/read_cache.py:/app/read_cache.py" \
     -v "$(pwd)/translate_specs.py:/app/translate_specs.py" \
     cbmc:latest "${CMD[@]}"
 fi
