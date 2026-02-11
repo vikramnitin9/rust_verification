9e286e2ba57d0160196191843770ee2bc3567df9
James Yoo <jyoo980@gmail.com>
2026-02-11 09:47:49 -0800

    Adding first regression test

---
+++ data/cbmc_regression/assigns_enforce_08/main_generated.c
@@ -0,0 +1,24 @@
+void f2(int** y)\n__CPROVER_requires(__CPROVER_is_fresh(y, sizeof(int*)))
+__CPROVER_requires(__CPROVER_is_fresh(*y, sizeof(int)))
+__CPROVER_ensures(**y == 5)
+__CPROVER_assigns(**y)
+{
+  **y = 5;
+}
+
+void f1(int *x)
+__CPROVER_requires(__CPROVER_is_fresh(x, sizeof(*x)))
+__CPROVER_ensures(*x == 5)
+__CPROVER_assigns(*x)
+{
+  int *a = x;
+  f2(&a);
+}
+
+int main()
+{
+  int n = 3;
+  f1(&n);
+
+  return 0;
+}
