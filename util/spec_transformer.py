15e067de25463a45f54838d5931629675e7c556c
Michael Ernst <mernst@cs.washington.edu>
2026-02-19 16:57:37 -0800

    Tweak comments

--- util/spec_transformer.py
+++ util/spec_transformer.py
@@ -119,7 +119,7 @@ class SpecTransformer:
             }
             ```
 
-        It is the case that `f1` might be called with a NULL `pp` (or `pp->p` or `pp->p->buf`), and
+        Function `f1` might be called with a NULL `pp` (or `pp->p` or `pp->p->buf`), and
         the function accounts for this with a conditional check. A more expressive spec is:
 
             __CPROVER_ensures(
@@ -181,7 +181,7 @@ class SpecTransformer:
 
         Args:
             exprs (list[CBMCAst]): The exprs to which to apply the logical operation.
-            logical_op (type[AndOp  |  OrOp]): The logical operation.
+            logical_op (type[AndOp | OrOp]): The logical operation.
 
         Returns:
             CBMCAst: The result of applying the logical operation.
