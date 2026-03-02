be68b9ae790662f7e33837f6ac8044b0b5288371
Vikram Nitin <vikramnitin9@gmail.com>
2026-03-02 16:42:52 -0500

    Extension to handle projects with multiple files (#92)
    Co-authored-by: James Yoo <jyoo980@gmail.com>
    Co-authored-by: Michael Ernst <mernst@cs.washington.edu>

--- util/__init__.py
+++ util/__init__.py
@@ -1,10 +1,10 @@
 from .specification_generation_next_step import SpecificationGenerationNextStep, AcceptVerifiedSpec, AssumeSpecAsIs, BacktrackToCallee
 from .code_extraction_util import extract_function_source_code, parse_clauses_for_spec, parse_expressions_for_spec
-from .file_util import copy_file_to_folder, ensure_lines_at_beginning
-from .function_util import extract_specification
+from .file_util import copy_file_to_folder, copy_folder_to_folder, ensure_lines_at_beginning
+from .function_util import extract_specification, get_source_content_with_specifications
 from .parsec_error import ParsecError
 from .c_function import CFunction
-from .parsec_file import ParsecFile
+from .parsec_project import ParsecProject
 from .function_specification import FunctionSpecification
 from .specification.specgen_granularity import SpecGenGranularity
 from .spec_conversation import SpecConversation
@@ -22,12 +22,14 @@ __all__ = [
     "FunctionSpecification",
     "SpecConversation",
     "ParsecError",
-    "ParsecFile",
+    "ParsecProject",
     "copy_file_to_folder",
+    "copy_folder_to_folder",
     "extract_function_source_code",
     "parse_clauses_for_spec",
     "parse_expressions_for_spec",
     "extract_specification",
+    "get_source_content_with_specifications",
     "ensure_lines_at_beginning",
     "parse_object",
     "parse_backtracking_info",
