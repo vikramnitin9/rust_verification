c50bc080d4739a684e288ba043f2ca847d67819b
James Yoo <jyoo980@gmail.com>
2026-03-04 17:11:33 -0800

    Adding `Mutable` protocol

---
+++ translation/ast/mutable.py
@@ -0,0 +1,17 @@
+"""Class to represent AST nodes from which mutant nodes can be generated."""
+
+from typing import Protocol
+
+from .cbmc_ast import CBMCAst
+
+
+class Mutable(Protocol):
+    """Class to represent AST nodes from which mutant nodes can be generated."""
+
+    def get_mutation_candidates(self) -> list[type[CBMCAst]]:
+        """Return the type(s) of CBMCAst nodes that this node may be mutated into.
+
+        Returns:
+            list[type[CBMCAst]]: The type(s) of CBMCAst nodes that this node may be mutated into.
+        """
+        ...
