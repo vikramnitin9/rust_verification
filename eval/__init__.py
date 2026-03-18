473b8fd6129e6b67a0c65ff4faa8d815b0293f37
James Yoo <jyoo980@gmail.com>
2026-03-17 17:39:38 -0700

    Fixing import error

--- eval/__init__.py
+++ eval/__init__.py
@@ -1,3 +1,3 @@
-from .spec_complexity_util import is_tautology, count_atoms_in_expression, get_complexity, ClauseComplexityInfo, get_atoms_in_expression
+from .spec_complexity_util import is_tautology, get_complexity, ClauseComplexityInfo, get_atoms_in_expression
 
-__all__ = ["ClauseComplexityInfo", "get_complexity", "count_atoms_in_expression", "is_tautology", "get_atoms_in_expression"]
+__all__ = ["ClauseComplexityInfo", "get_complexity", "is_tautology", "get_atoms_in_expression"]
