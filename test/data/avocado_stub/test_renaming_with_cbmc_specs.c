3195a4a5720c69b3bca767a6a37d269a7dabc302
James Yoo <24359440+jyoo980@users.noreply.github.com>
2026-04-15 13:42:14 -0700

    Apply Avocado stub renaming to client code (i.e., code under verification) (#124)
    Co-authored-by: Michael Ernst <mernst@cs.washington.edu>

--- test/data/avocado_stub/test_renaming_with_cbmc_specs.c
+++ test/data/avocado_stub/test_renaming_with_cbmc_specs.c
@@ -4,6 +4,8 @@
 #include <time.h> // `time` is renamed function name, but this header should not be renamed.
 
 int is_separator(int c)
+__CPROVER_ensures(__CPROVER_return_value == (c == '\0' || isspace(c) || strchr(",.()+-/*=~%[];",c) != NULL))
+__CPROVER_ensures((__CPROVER_return_value == 0) ==> (c == '\0' || isspace(c) || strchr(",.()+-/*=~%[];",c) == NULL))
 {
     return c == '\0' ||
         isspace(c) ||
