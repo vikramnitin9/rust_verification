3d32c5512375f75413a17bec09895ec63baf88ca
James Yoo <jyoo980@gmail.com>
2026-04-20 13:55:27 -0700

    Tweaks

--- main.py
+++ main.py
@@ -341,7 +341,7 @@ def _verify_program(
                 continue
             GLOBAL_OBSERVED_PROOFSTATES.add(next_proofstate)
 
-            if next_proofstate.is_workstack_empty() == 0:
+            if next_proofstate.is_workstack_empty():
                 GLOBAL_COMPLETE_PROOFSTATES.append(next_proofstate)
             else:
                 GLOBAL_INCOMPLETE_PROOFSTATES.append(next_proofstate)
